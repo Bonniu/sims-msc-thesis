0c929dfecd9665bb9c1bf62729e2bd428f836a26
Bonniu <radoslaw.grela@gmail.com>
2022-11-26 13:00:55 +0100

    initial commit for logsmock

---
+++ logsmock/src/main/java/com/logsmock/logsmock/LogsmockApplication.java
@@ -0,0 +1,13 @@
+package com.logsmock.logsmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsmockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsmockApplication.class, args);
+	}
+
+}
---
+++ logsmock/src/test/java/com/logsmock/logsmock/LogsmockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logsmock.logsmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsmockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
