63751cc999b3fa9aabad6b56ea26649bea1249cb
Bonniu <radoslaw.grela@gmail.com>
2022-11-26 23:11:04 +0100

    logsmock - several users with many random actions

--- logsmock/src/main/java/com/logsmock/logsmock/LogsmockApplication.java
+++ logsmock/src/main/java/com/logsmock/logsmock/LogsmockApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.text.MessageFormat;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.logsmock.logsmock.LogMessages.*;
 import static com.logsmock.logsmock.TimeCalculator.addRandomnessToTime;
@@ -22,55 +21,63 @@ public class LogsmockApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LogsmockApplication.class, args);
-        generateNormalUser("jkiwior", 8, 0, 8);
+        var list = new ArrayList<MyLog>();
+
+        list.addAll(generateNormalUser("jkiwior", 8, 0, 8));
+        list.addAll(generateNormalUser("rlewandowski", 7, 30, 8));
+        list.addAll(generateNormalUser("wszczesny", 9, 0, 6));
+        list.addAll(generateNormalUser("gkrychowiak", 12, 0, 8));
+        list.addAll(generateNormalUser("pzielinski", 8, 0, 12));
+        list.addAll(generateNormalUser("kpiatek", 9, 15, 6));
+
+        list.sort(MyLog::compareTo);
+        for (var t : list) {
+            System.out.println(t);
+        }
 
     }
 
-    public static void generateNormalUser(String username, int startHour, int startMinutes, int workTime) {
-        int requestperiod = 15; // minutes
+    public static List<MyLog> generateNormalUser(String username, int startHour, int startMinutes, int workTime) {
+        int requestPeriod = 15; // minutes
 
         var list = new ArrayList<MyLog>();
         // days between 14-27 november
         for (int i = 14; i < 28; i++) {
             if (i == 19 || i == 20) continue; // weekend
             var today = LocalDate.of(2022, 11, i);
+
             // start work
             var startTime = addRandomnessToTime(startHour, startMinutes, 3);
             list.add(new MyLog(today, startTime, username, MessageFormat.format(LOGGED_TO_WORK, username)));
-
-            //work time
-            var time = startTime.plus(15, ChronoUnit.MINUTES);
-            list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
-            time = time.plus(827, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
-            time = time.plus(302, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
-            time = time.plus(47, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(RESERVATION, username)));
-            time = time.plus(1503, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
-            time = time.plus(7815, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
-            time = time.plus(65, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(RESERVATION, username)));
-            time = time.plus(650, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
-            time = time.plus(15, ChronoUnit.SECONDS);
-            list.add(new MyLog(today, time, username, MessageFormat.format(RESERVATION, username)));
-
 
             //end work
             var endTime = addRandomnessToTime(startHour + workTime, startMinutes + 5, 2);
             String s = countTime(startTime, endTime);
             list.add(new MyLog(today, endTime, username, MessageFormat.format(LOGGED_OUT, username, s)));
 
+            //between
+            var time = startTime.plus(15, ChronoUnit.MINUTES);
+            list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
+
+            while (time.isBefore(endTime)) {
+                time = time.plus(randomInt(30, requestPeriod * 60), ChronoUnit.SECONDS);
+                if (time.isAfter(endTime)) break;
+                list.add(new MyLog(today, time, username, MessageFormat.format(ALL_RESERVATIONS, username)));
+                if (randomInt(1, 100) % 2 == 0) {
+                    time = time.plus(randomInt(30, 100), ChronoUnit.SECONDS);
+                    if (time.isAfter(endTime)) break;
+                    list.add(new MyLog(today, time, username, MessageFormat.format(RESERVATION, username)));
+                }
+            }
+
         }
 
-        for (var t : list) {
-            System.out.println(t);
-        }
 
+        return list;
+    }
 
+    public static int randomInt(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
     }
 
 }
--- logsmock/src/main/java/com/logsmock/logsmock/MyLog.java
+++ logsmock/src/main/java/com/logsmock/logsmock/MyLog.java
@@ -1,17 +1,50 @@
 package com.logsmock.logsmock;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Data
+@Getter
 @AllArgsConstructor
-public class MyLog {
+public class MyLog implements Comparable<MyLog> {
 
     private LocalDate date;
     private LocalTime time;
     private String username;
     private String message;
+    private String type;
+
+    public MyLog(LocalDate date, LocalTime time, String username, String message) {
+        this(date, time, username, message, " INFO");
+    }
+
+    @Override
+    public int compareTo(MyLog b) {
+        if (this.getDate().isEqual(b.getDate())) {
+            if (this.getTime().equals(b.getTime())) {
+                return 0;
+            }
+            return this.getTime().isBefore(b.getTime()) ? -1 : 1;
+        }
+        return this.getDate().isBefore(b.getDate()) ? -1 : 1;
+
+    }
+
+    @Override
+    public String toString() {
+        return
+                getDate() + "T" + resolveTimeString() + ".000+01:00 " //date and time
+                        + getType() // log type
+                        + " 17948 --- [           main] c.logsmock.logsmock.LogsmockApplication  : " // random, not used
+                        + getMessage(); // message
+    }
+
+    private String resolveTimeString() {
+        if (getTime().toString().length() < 8)
+            return getTime().toString() + ":00";
+        else
+            return getTime().toString();
+    }
 }
